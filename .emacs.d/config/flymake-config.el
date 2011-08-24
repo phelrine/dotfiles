64685bc83e9eba4a1303702750f138980b2a5d87
phelrine <phelrine@gmail.com>
2011-08-24 12:11:51 +0900

    python flymake setting

--- .emacs.d/config/flymake-config.el
+++ .emacs.d/config/flymake-config.el
@@ -1,6 +1,7 @@
 (require 'flymake)
+(require 'flymake-cursor)
 
-(set-face-background 'flymake-errline "red4")
+(set-face-background 'flymake-errline "red")
 (set-face-background 'flymake-warnline "dark slate blue")
 
 ;; Invoke ruby with '-c' to get syntax checking
@@ -15,11 +16,29 @@
 (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
 (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
 
+(defun flymake-pyflakes-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		     'flymake-create-temp-inplace))
+	 (local-file (file-relative-name
+		      temp-file
+		      (file-name-directory buffer-file-name))))
+    (list "pyflakes"  (list local-file))))
+(push '("\\.py\\'" flymake-pyflakes-init) flymake-allowed-file-name-masks)
+
+(mapc (lambda (hook) (add-hook hook 'flymake-find-file-hook))
+      '(python-mode-hook
+	ruby-mode-hook))
+
 (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-	     (if (and (not (null buffer-file-name)) 
-		      (file-writable-p buffer-file-name))
-		 (flymake-mode))))
+
+
+
+
+
+
+
+
+
+
 
