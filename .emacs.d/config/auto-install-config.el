58600ca55ed538ae304c415d366168d254154306
phelrine <phelrine@gmail.com>
2011-09-07 12:17:55 +0900

    flymake-cursor

--- .emacs.d/config/auto-install-config.el
+++ .emacs.d/config/auto-install-config.el
@@ -3,8 +3,9 @@
 (setq auto-install-directory "~/.emacs.d/site-lisp/")
 (auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)
-()
+
 ;; (auto-install-batch "anything")
 ;; (auto-install-batch "auto-complete development version")
 ;; (auto-install-from-emacswiki "yasnippet-config.el")
 ;; (auto-install-from-emacswiki "auto-async-byte-compile.el")
+;; (auto-install-from-emacswiki "flymake-cursor.el")
