aa086b9f8d15dd3a0eb20c96cf020d6b79af522e
phelrine <phelrine@gmail.com>
2011-10-12 00:10:04 +0900

    twmodeで慣性スクロール

--- .emacs.d/config/twittering-config.el
+++ .emacs.d/config/twittering-config.el
@@ -4,3 +4,14 @@
 (setq twittering-icon-mode t)
 (setq twittering-scroll-mode t)
 (setq twittering-display-remaining t)
+
+(require 'inertial-scroll)
+(add-hook 'twittering-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<wheel-up>") 'inertias-down-wheel)
+            (local-set-key (kbd "<wheel-down>") 'inertias-up-wheel)
+            (local-set-key (kbd "<mouse-4>") 'inertias-down-wheel)
+            (local-set-key (kbd "<mouse-5>") 'inertias-up-wheel)
+            (local-set-key (kbd "M-v") 'inertias-down)
+            (local-set-key (kbd "C-v") 'inertias-up)
+            ))
