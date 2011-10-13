7336cf6e18cf4595df699bf98e9415fe87ebadd0
phelrine <phelrine@gmail.com>
2011-10-13 17:04:36 +0900

    flymake

--- .emacs.d/config/cc-config.el
+++ .emacs.d/config/cc-config.el
@@ -1,21 +1,3 @@
 (require 'hideshow)
+(add-hook 'c-mode-common-hook (lambda () (hs-minor-mode t)))
 
-(require 'flymake)
-(defun flymake-local-file ()
-  (file-relative-name (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace)
-                      (file-name-directory buffer-file-name)))
-
-(defun flymake-c-init ()
-  (list "gcc" (list "-Wall" "-Wextra" "-fsyntax-only" (flymake-local-file))))
-
-(defun flymake-c++-init ()
-  (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" (flymake-local-file))))
-
-;; (push '(".+\\.c$" flymake-c-init) flymake-allowed-file-name-masks)
-;; (push '(".+\\.cc$" flymake-c++-init) flymake-allowed-file-name-masks)
-
-(dolist (hook '(c++-mode-hook c-mode-hook))
-  (add-hook hook (lambda ()
-                   (hs-minor-mode t)
-                   (flymake-mode t)
-                   )))
--- .emacs.d/config/flymake-config.el
+++ .emacs.d/config/flymake-config.el
@@ -1,25 +1,20 @@
 (require 'flymake)
 (require 'flymake-cursor)
 
-(set-face-background 'flymake-errline "red")
-(set-face-background 'flymake-warnline "dark slate blue")
+(set-face-background 'flymake-errline "tomato")
+(set-face-background 'flymake-warnline "gold")
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
 (defun flymake-pyflakes-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		     'flymake-create-temp-inplace))
-	 (local-file (file-relative-name
-		      temp-file
-		      (file-name-directory buffer-file-name))))
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
     (list "pyflakes"  (list local-file))))
 (push '("\\.py\\'" flymake-pyflakes-init) flymake-allowed-file-name-masks)
-
-(mapc (lambda (hook) (add-hook hook 'flymake-find-file-hook))
-      '(python-mode-hook
-	ruby-mode-hook))
-
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-
-
 
 
 
