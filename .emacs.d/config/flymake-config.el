a5da34a8779303cec92e87e78211c21cc247ce0f
phelrine <phelrine@gmail.com>
2011-10-16 14:27:34 +0900

    flymake gcc4.5対応

--- .emacs.d/config/flymake-config.el
+++ .emacs.d/config/flymake-config.el
@@ -1,11 +1,15 @@
 (require 'flymake)
 (require 'flymake-cursor)
 
-(set-face-background 'flymake-errline "tomato")
-(set-face-background 'flymake-warnline "gold")
+(set-face-background 'flymake-errline "red")
+(set-face-background 'flymake-warnline "yellow")
+
+(setq flymake-err-line-patterns
+      `(("\\(.+\\):\\([0-9]+\\):\\([0-9]+\\): \\(.+\\)" 1 2 3 4) ; gcc 4.5
+        ("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)          ; ruby
+        ,@flymake-err-line-patterns))
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
 (defun flymake-pyflakes-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -14,7 +18,6 @@
                       temp-file
                       (file-name-directory buffer-file-name))))
     (list "pyflakes"  (list local-file))))
+
 (push '("\\.py\\'" flymake-pyflakes-init) flymake-allowed-file-name-masks)
-
-
 
