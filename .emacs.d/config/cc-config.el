0c6003ac17bd8a2118b29c9497984ce8c20d2850
phelrine <phelrine@gmail.com>
2012-02-06 21:42:22 +0900

    remove emacs config

--- .emacs.d/config/cc-config.el
+++
@@ -1,7 +0,0 @@
-(require 'hideshow)
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (add-to-list 'ac-sources 'ac-source-semantic)
-            (local-set-key (kbd "C-c c") 'compile)
-            (hs-minor-mode t)))
